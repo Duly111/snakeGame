ba144a91cac7d43ab1b4755ed0e7964189783692
Ivaylo Radulov <164617687+Duly111@users.noreply.github.com>
2024-11-09 19:23:21 +0200

    add if state to prevent the snake from turning in the opposite direction it is moving and the game ending

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ document.body.addEventListener('keydown',keyDown);
 function keyDown(event){
     //up 
     if (event.keyCode == 38){
+        if(yVelocity == 1)
+            return;
         yVelocity = -1;
         xVelocity = 0;
     }
@@ -46,18 +48,24 @@ function keyDown(event){
     //down
 
     if (event.keyCode == 40){
+        if(yVelocity == -1)
+            return;
         yVelocity = 1;
         xVelocity = 0;
     }
 
     //left
     if (event.keyCode == 37){
+        if(xVelocity == 1)
+            return;
         yVelocity = 0;
         xVelocity = -1;
     }
 
     //right
     if (event.keyCode == 39){
+        if(xVelocity == -1)
+            return;
         yVelocity = 0;
         xVelocity = 1;
     }
